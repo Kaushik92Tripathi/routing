25896154fbdd58cd77f9912d93492b32eba0a1d5
Kaushik92Tripathi <kaushik.tripathi@tothenew.com>
2025-02-22 18:53:57 +0530

    added logo

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,25 @@
 import Link from "next/link";
+import { Layout } from "lucide-react";
 
 export default function Header() {
-    return (
-      <header className="bg-blue-700 text-white p-4 shadow-md flex justify-between items-center">
-        <Link href='/'>
-        
-        <h1 className="text-2xl font-bold">My Awesome App</h1>
-        </Link>
-        <nav>
-          <ul className="flex space-x-4">
-            <li>
-              <a href="/contact" className="hover:underline">Contact</a>
-            </li>
-            <li>
-              <a href="/about" className="hover:underline">About</a>
-            </li>
-            <li>
-              <a href="/products" className="hover:underline">Products</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
+  return (
+    <header className="bg-blue-700 text-white p-4 shadow-md flex justify-between items-center">
+      <Link href='/' className="flex items-center">
+        <Layout size={32} className="text-white" />
+      </Link>
+      <nav>
+        <ul className="flex space-x-4">
+          <li>
+            <a href="/contact" className="hover:underline">Contact</a>
+          </li>
+          <li>
+            <a href="/about" className="hover:underline">About</a>
+          </li>
+          <li>
+            <a href="/products" className="hover:underline">Products</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
